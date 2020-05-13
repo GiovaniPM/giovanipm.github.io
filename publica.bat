1d53d07dd1f7401ddde8c6aa7a595f10fade04cc
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-05-13 19:23:45 -0300

    giova(DESKTOP-PG18CKU) 200513192345

---
+++ publica.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set ant=%cd%
+
+echo (1/7) Gerando pacote
+echo (1/7) Gerando pacote > %ant%\log.txt
+tar --exclude="./.git" --exclude="./gpm.ktz" --exclude="./home.000" --exclude="./log.txt" -cvf home.000 .
+
+echo (2/7) Movendo pacote
+echo (2/7) Movendo pacote >> %ant%\log.txt
+xcopy home.000 ..\help\docker\home\. /y >> %ant%\log.txt
+del home.000 >> %ant%\log.txt
+
+echo (3/7) Aplicando container
+echo (3/7) Aplicando container >> %ant%\log.txt
+docker stop home >> %ant%\log.txt
+
+echo (4/7) Removendo container
+echo (4/7) Removendo container >> %ant%\log.txt
+docker image rm home >> %ant%\log.txt
+
+echo (5/7) Removendo imagem
+echo (5/7) Removendo imagem >> %ant%\log.txt
+docker rm home >> %ant%\log.txt
+cd ..\help\docker\home
+
+echo (6/7) Criando imagem
+echo (6/7) Criando imagem >> %ant%\log.txt
+docker build --tag=home . >> %ant%\log.txt
+
+echo (7/7) Criando container
+echo (7/7) Criando container >> %ant%\log.txt
+docker run -d --name="home" -p 8081:80 home >> %ant%\log.txt
+
+cd %ant%
